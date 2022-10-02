9eb776ab40a19c36153dd7af54edc8a217e18fa8
LilyLiuweb <phliu814@gmail.com>
2022-10-02 17:57:45 +0800

    owl_carousel add outer JS file.

---
+++ mainPage.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplaytimeout:1000,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:3
+        },
+        1200:{
+            items:3
+        }
+    }
+})
+var headerBg = document.getElementById('carouselExampleIndicators')
+window.addEventListener('scroll',function(){
+    headerBg.style.opacity = 1 - +window.pageXOffset/550+'';
+})
